333094e2d0d759855ba2c3f3c83f71e2c6ebdaba
Oleksandr Stefanovskyi <astefanovskiy@gmail.com>
2018-09-15 15:05:27 +0300

    Extract init data to class

--- src/main/java/com/stefanovskyi/gitdeceiver/GitDeceiver.java
+++ src/main/java/com/stefanovskyi/gitdeceiver/GitDeceiver.java
@@ -8,26 +8,68 @@ import java.time.Month;
 
 public class GitDeceiver {
     public static void main(String args[]) {
-        String repositoryName = args[0];
-        String userFullName = args[1];
-        String userEmail = args[2];
-        String login = args[3];
-        String password = args[4];
+        GitAuthData gitAuthData = new GitAuthData(args).invoke();
 
         FakeRepository fakeRepository = new FakeRepository();
 
-        fakeRepository.cloneRepository(repositoryName);
-        fakeRepository.addRemote(repositoryName);
-        LocalDateTime commitDate = LocalDateTime.of(2017, Month.SEPTEMBER, 4, 5, 15);
+        fakeRepository.cloneRepository(gitAuthData.getRepositoryName());
+        fakeRepository.addRemote(gitAuthData.getRepositoryName());
+        LocalDateTime commitDate = getStartDate();
 
         for (int i = 0; i < 10; i++) {
             String date = LocalDateTime.now().toString();
             commitDate = commitDate.plusDays(1);
-            PersonIdent personIdent = fakeRepository.getPersonIdent(userFullName, userEmail, commitDate);
+            PersonIdent personIdent = fakeRepository.getPersonIdent(gitAuthData.getUserFullName(), gitAuthData.getUserEmail(), commitDate);
             fakeRepository.makeFakeCommit(personIdent, date);
         }
 
-        fakeRepository.pushToRemote(login, password);
-        Util.deleteRepositoryFolderIfExists(Util.getRepositoryNameFromUrl(repositoryName));
+        fakeRepository.pushToRemote(gitAuthData.getLogin(), gitAuthData.getPassword());
+        Util.deleteRepositoryFolderIfExists(Util.getRepositoryNameFromUrl(gitAuthData.getRepositoryName()));
+    }
+
+    private static LocalDateTime getStartDate() {
+        return LocalDateTime.of(2017, Month.SEPTEMBER, 4, 5, 15);
+    }
+
+    private static class GitAuthData {
+        private String[] args;
+        private String repositoryName;
+        private String userFullName;
+        private String userEmail;
+        private String login;
+        private String password;
+
+        public GitAuthData(String... args) {
+            this.args = args;
+        }
+
+        public String getRepositoryName() {
+            return repositoryName;
+        }
+
+        public String getUserFullName() {
+            return userFullName;
+        }
+
+        public String getUserEmail() {
+            return userEmail;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public GitAuthData invoke() {
+            repositoryName = args[0];
+            userFullName = args[1];
+            userEmail = args[2];
+            login = args[3];
+            password = args[4];
+            return this;
+        }
     }
 }
