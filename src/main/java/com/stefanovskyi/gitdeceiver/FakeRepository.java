021f16cffd08c2a07784c5677543ffb6e737e085
Oleksandr Stefanovskyi <astefanovskiy@gmail.com>
2018-10-17 01:03:12 +0300

    Refactoring move Git related methods to appropriate class

--- src/main/java/com/stefanovskyi/gitdeceiver/FakeRepository.java
+++ src/main/java/com/stefanovskyi/gitdeceiver/FakeRepository.java
@@ -11,9 +11,11 @@ import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import java.net.URISyntaxException;
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Random;
 import java.util.TimeZone;
 
 public class FakeRepository {
@@ -44,8 +46,50 @@ public class FakeRepository {
             LOGGER.error(e.getMessage(), e);
         }
     }
+    public void generateCommits(GitAuthData gitAuthData, FakeRepository fakeRepository,
+                                        LocalDateTime commitDate, int amountOfDays) {
+        Random rand = new Random();
 
-    public void makeFakeCommit(PersonIdent commitAuthor) {
+        for (int day = 0; day < amountOfDays; day++) {
+            commitDate = commitDate.plusDays(1);
+
+            int commitsPerDay = getCommitsAmountPerDay(commitDate, rand);
+
+            for (int commitNumber = 0; commitNumber < commitsPerDay; commitNumber++) {
+
+                commitDate = commitDate.plusMinutes(15);
+                PersonIdent personIdent = fakeRepository.getPersonIdent(gitAuthData.getUserFullName(),
+                        gitAuthData.getUserEmail(),
+                        commitDate);
+                fakeRepository.makeFakeCommit(personIdent);
+            }
+        }
+    }
+
+    private int getCommitsAmountPerDay(LocalDateTime commitDate, Random rand) {
+        int maximumCommitsPerWeekDay = 6;
+        int maximumCommitsPerWeekend = 15;
+        int minimumCommitsPerWeekend = 8;
+        int fridayMaximumCommits = 10;
+        int fridayMinimumCommits = 6;
+
+        DayOfWeek dayOfWeek = commitDate.getDayOfWeek();
+
+        int commitsPerDay;
+
+        if (dayOfWeek.getValue() == 7 || dayOfWeek.getValue() == 6) {
+            commitsPerDay = rand.nextInt(maximumCommitsPerWeekend) + minimumCommitsPerWeekend;
+        } else if (dayOfWeek.getValue() == 5) {
+
+            commitsPerDay = rand.nextInt(fridayMaximumCommits) + fridayMinimumCommits;
+        }
+        else {
+            commitsPerDay = rand.nextInt(maximumCommitsPerWeekDay) + 1;
+        }
+        return commitsPerDay;
+    }
+
+    private void makeFakeCommit(PersonIdent commitAuthor) {
         Repository repository = this.git.getRepository();
         String fileName = "Main.java";
         try {
--- src/main/java/com/stefanovskyi/gitdeceiver/GitDeceiver.java
+++ src/main/java/com/stefanovskyi/gitdeceiver/GitDeceiver.java
@@ -1,11 +1,8 @@
 package com.stefanovskyi.gitdeceiver;
 
 import com.stefanovskyi.gitdeceiver.util.Util;
-import org.eclipse.jgit.lib.PersonIdent;
 
-import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.util.Random;
 
 public class GitDeceiver {
 
@@ -20,52 +17,9 @@ public class GitDeceiver {
 
         int amountOfDays = 333;
 
-        generateCommits(gitAuthData, fakeRepository, commitDate, amountOfDays);
+        fakeRepository.generateCommits(gitAuthData, fakeRepository, commitDate, amountOfDays);
 
         fakeRepository.pushToRemote(gitAuthData.getLogin(), gitAuthData.getPassword());
         Util.deleteRepositoryFolderIfExists(Util.getRepositoryNameFromUrl(gitAuthData.getRepositoryName()));
-    }
-
-    private static void generateCommits(GitAuthData gitAuthData, FakeRepository fakeRepository,
-                                        LocalDateTime commitDate, int amountOfDays) {
-        Random rand = new Random();
-
-        for (int day = 0; day < amountOfDays; day++) {
-            commitDate = commitDate.plusDays(1);
-
-            int commitsPerDay = getCommitsAmountPerDay(commitDate, rand);
-
-            for (int commitNumber = 0; commitNumber < commitsPerDay; commitNumber++) {
-
-                commitDate = commitDate.plusMinutes(15);
-                PersonIdent personIdent = fakeRepository.getPersonIdent(gitAuthData.getUserFullName(),
-                        gitAuthData.getUserEmail(),
-                        commitDate);
-                fakeRepository.makeFakeCommit(personIdent);
-            }
-        }
-    }
-
-    private static int getCommitsAmountPerDay(LocalDateTime commitDate, Random rand) {
-        int maximumCommitsPerWeekDay = 6;
-        int maximumCommitsPerWeekend = 15;
-        int minimumCommitsPerWeekend = 8;
-        int fridayMaximumCommits = 10;
-        int fridayMinimumCommits = 6;
-
-        DayOfWeek dayOfWeek = commitDate.getDayOfWeek();
-
-        int commitsPerDay;
-
-        if (dayOfWeek.getValue() == 7 || dayOfWeek.getValue() == 6) {
-            commitsPerDay = rand.nextInt(maximumCommitsPerWeekend) + minimumCommitsPerWeekend;
-        } else if (dayOfWeek.getValue() == 5) {
-
-            commitsPerDay = rand.nextInt(fridayMaximumCommits) + fridayMinimumCommits;
-        }
-        else {
-            commitsPerDay = rand.nextInt(maximumCommitsPerWeekDay) + 1;
-        }
-        return commitsPerDay;
     }
 }
